4791f91e4e85a27895eb293aa90b2917a6b8f574
Liesel <marceloschloymann@gmail.com>
2024-07-08 12:32:51 -0300

    agregue sku

--- src/componentes/Productos.jsx
+++ src/componentes/Productos.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Productos.css";
 
-function Productos({ nombre, descripcion, imagen, precio }) {
+function Productos({ nombre, descripcion, imagen, precio, sku }) {
 
     return (
         <article className="productosCard">
             
             <img src={imagen} alt="logo" width={200}/>
             <h4>{nombre}</h4>
+            <p>{sku}</p>
             <p>{precio}</p>
             <h5>Caracteristicas del Producto</h5>
             <p>{descripcion}</p>
